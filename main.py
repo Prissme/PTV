579d0407fad6a60d51b5c50c044d2a8ff3070b0e
Prissme <drissbouhaiat@gmail.com>
2026-09-07 23:33:09 +0200

    Remove 'enchantments' from command list
    Removed 'enchantments' from the command list and ensured proper formatting.

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ class EcoBot(commands.Bot):
             "drops",
             "pets",
             "potions",
-            "enchantments",
             "help",
             "plaza",
             "admin",
